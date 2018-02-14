0b83c48bf156c57f38160e631b22090260c0cb60
Bridson.Dylan <Bridson.Dylan@SEMS-259-34.SEMS259.LAB>
2018-02-14 14:54:30 -0800

    Valentines day thing

---
+++ src/methods/ValentinesDay.java
@@ -0,0 +1,35 @@
+package methods;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class ValentinesDay {
+public static void main(String[] args) {
+Robot Jeff=new Robot("batman");
+Jeff.setPenColor(234,123,187);
+
+Jeff.setSpeed(111);Jeff.penDown();
+Jeff.move(200);
+Jeff.turn(90);
+Jeff.move(100);
+Jeff.turn(90);
+Jeff.move(200);
+Jeff.turn(45);
+Jeff.move(150);
+Jeff.turn(90);
+Jeff.move(150);
+Jeff.turn(45);
+Jeff.move(200);
+Jeff.turn(90);
+Jeff.move(100);
+
+
+
+
+
+	
+} 
+	
+	
+}
